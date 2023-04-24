d0b6cc274ab392fa7d2ba5b26623c628a4613928
basha <aelnegaralaa@gmailcom>
2023-04-23 23:43:07 -0700

    d

--- string_fs.c
+++ string_fs.c
@@ -10,21 +10,21 @@
  */
 char *get_precision(char *p, params_t *params, va_list ap)
 {
-        int sam = 0;
+	int d = 0;
 
-        if (*p != '.')
-                return (p);
-        p++;
-        if (*p == '*')
-        {
-                sam = va_arg(ap, int);
-                p++;
-        }
-        else
-        {
-                while (_isdigit(*p))
-                        sam = sam * 10 + (*p++ - '0');
-        }
-        params->precision = sam;
-        return (p);
+	if (*p != '.')
+		return (p);
+	p++;
+	if (*p == '*')
+	{
+		d = va_arg(ap, int);
+		p++;
+	}
+	else
+	{
+		while (_isdigit(*p))
+			d = d * 10 + (*p++ - '0');
+	}
+	params->precision = d;
+	return (p);
 }
